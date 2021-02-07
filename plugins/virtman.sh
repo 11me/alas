f90c1eea90418f9bbf11b0b9ad8b1e3b56aafa5b
11me <osmium760@gmail.com>
2021-02-08 01:49:38 +0300

    rm from master

--- plugins/virtman.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-
-# Script for installing virtual-manager and qemu
-# Run this script as sudo
-pacman -S --noconfirm --needed qemu virt-manager libvirt ebtables dnsmasq \
-    bridge-utils edk2-ovmf
-usermod -aG kvm libvirt lime
